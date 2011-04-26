6a89917c2bd8c01e175c991b20996b6bec6dec23
pgilbert <pgilbert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-26 12:59:28 +0000

    added expand and extract utilities for converting daily, monthly quarterly data.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsanalysis@51 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tfMiscDevel/R/devel.R
+++ pkg/tfMiscDevel/R/devel.R
@@ -39,3 +39,99 @@ tfOne<- function(x, tf=tframe(x), start=tfstart(tf), end=tfend(tf),
     invisible(x)
     }
 
+############################################
+ 
+# Note that there is no need to account for the observation lag, the data is
+#  entered as NA if it is not available, and the filter fills it in.
+
+expandMtoW <- function(x, fromStart=start(x), notreleased=-7000, na=-99999){
+     # assign monthly data into the last Friday of a month to 
+     # give a weekly Friday series with na inserted where there is no data.
+     # notreleased is for values which will eventually be available.
+   if (12 != frequency(x)) stop("data must be monthly.")
+   x <- tfwindow(x, start=fromStart)
+   require("zoo")
+
+   #as.Date(0) = Thursday Jan 1, 1970
+   # from 1970 to end of x + a bit
+   fridays <- as.Date(1) + 7 *  0:(53* (1+ end(x)[1] - 1970) )
+   # window to span of x
+   st <-  (year(fridays)  < start(x)[1]) | 
+         ((year(fridays) == start(x)[1]) & (month(fridays) < start(x)[2]))
+   en <-  (year(fridays)  > end(x)[1])   | 
+         ((year(fridays) == end(x)[1])   & (month(fridays) > end(x)[2]))
+   
+   fridays <- fridays[(!st) & (!en)]
+   mn <- c(month(fridays), 13) # 13 to insure last obs is included
+   # logical indicating last Fridays of the month, plus the last 
+   Wind <- mn[-1] != mn[-length(mn)]
+   # lastFridays <- fridays[Wind]
+
+   x[is.na(x)] <- notreleased 
+   
+   r <- matrix(NA, Tobs(Wind), nseries(x))
+   r[Wind,] <- x
+   r[is.na(r)] <- na
+   r <- zoo(r, order.by=fridays)
+   seriesNames(r) <- seriesNames(x)
+   r
+   }
+
+expandQtoW <- function(x, fromStart, notreleased=-7000, na=-99999){
+     # assign quarterly data into the last Friday of a quarter to 
+     # give a weekly Friday series with na inserted where there is no data.
+     # notreleased is for values which will eventually be available.
+   if (4 != frequency(x)) stop("data must be quarterly.")
+   x <- tfwindow(x, start=fromStart)
+   require("zoo")
+
+   #as.Date(0) = Thursday Jan 1, 1970
+   # from 1970 to end of x + a bit
+   fridays <- as.Date(1) + 7 *  0:(53* (1+ end(x)[1] - 1970) )
+   # window to span of x
+   q <- ceiling(month(fridays)/3)
+   st <-  (year(fridays)  < start(x)[1]) | 
+         ((year(fridays) == start(x)[1]) & (q < start(x)[2]))
+   en <-  (year(fridays)  > end(x)[1])   | 
+         ((year(fridays) == end(x)[1])   & (q > end(x)[2]))
+   
+   fridays <- fridays[(!st) & (!en)] # fridays in range of x
+   q       <-     c(q[(!st) & (!en)], 5)# 5 to insure last obs is included
+   # logical indicating last Fridays of the quarter
+   Wind <- q[-1] != q[-length(q)]
+   # lastFridays <- fridays[Wind]
+
+   x[is.na(x)] <- notreleased 
+   
+   r <- matrix(NA, Tobs(Wind), nseries(x))
+   r[Wind,] <- x
+   r[is.na(r)] <- na
+   r <- zoo(r, order.by=fridays)
+   seriesNames(r) <- seriesNames(x)
+   r
+   }
+
+
+extractDtoW <- function(x, fromStart, notreleased=-7000, na=-99999){
+     # extract daily data from the last Friday of a week to 
+     # give a weekly Friday series with na inserted where there is no data.
+   if (1 != frequency(x)) stop("data must be daily.")
+   require("zoo")
+   x <- tfwindow(x, start=fromStart)
+   x[is.na(x)] <- notreleased # or na?
+
+   #as.Date(0) = Thursday Jan 1, 1970
+   # from 1970 to end of x + a bit
+   fridays <- as.Date(1) + 7 *  0:(53* (1+ year(end(x)) - 1970) )
+   # window to span of x
+   st <-  fridays  < time(x)[1]
+   en <-  fridays  > time(x)[Tobs(x)]
+   fridays <- fridays[(!st) & (!en)]
+
+   Wind <- time(x) %in% fridays
+      
+   r <- x[Wind,]
+   r <- zoo(r, order.by=fridays)
+   seriesNames(r) <- seriesNames(x)
+   r
+   }

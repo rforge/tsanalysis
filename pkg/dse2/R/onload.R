e77880c6e5c27ba6d43c2e39d0dd7c51c9e6e351
pgilbert <pgilbert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-11 02:23:26 +0000

    minor edit.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tsanalysis@130 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dse2/R/onload.R
+++ pkg/dse2/R/onload.R
@@ -1,4 +1,4 @@
-.onLoad  <- function(libname, pkgname) {
+.onAttach  <- function(libname, pkgname) {
    packageStartupMessage(
       "Package dse2 is deprecated. Please use packages dse and EvalEst instead.
      Some function previously in dse2 are now in dse, but many are in EvalEst.")
